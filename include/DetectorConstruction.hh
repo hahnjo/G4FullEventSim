53dcb50e5661c6f235059132c18c85ac921b676e
Jonas Hahnfeld <jonas.hahnfeld@cern.ch>
2021-05-17 17:24:04 +0200

    Add command to set uniform magnetic field

--- include/DetectorConstruction.hh
+++ include/DetectorConstruction.hh
@@ -3,18 +3,29 @@
 #ifndef DETECTOR_CONSTRUCTION_HH
 #define DETECTOR_CONSTRUCTION_HH
 
-#include <G4GDMLParser.hh>
+#include <G4ThreeVector.hh>
 #include <G4VUserDetectorConstruction.hh>
 
+#include <memory>
+#include <string>
+
+class DetectorConstructionMessenger;
+
 class DetectorConstruction final : public G4VUserDetectorConstruction {
+  friend class DetectorConstructionMessenger;
+
 public:
   DetectorConstruction();
   ~DetectorConstruction();
 
   G4VPhysicalVolume *Construct() override;
+  void ConstructSDandField() override;
 
 private:
-  G4GDMLParser fParser;
+  std::unique_ptr<DetectorConstructionMessenger> fMessenger;
+
+  std::string fGDMLfile;
+  G4ThreeVector fMagneticField;
 };
 
 #endif // DETECTOR_CONSTRUCTION_HH
--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -2,14 +2,87 @@
 
 #include "DetectorConstruction.hh"
 
-DetectorConstruction::DetectorConstruction() {}
+#include <G4FieldManager.hh>
+#include <G4GDMLParser.hh>
+#include <G4SystemOfUnits.hh>
+#include <G4ThreeVector.hh>
+#include <G4TransportationManager.hh>
+#include <G4UIcmdWith3VectorAndUnit.hh>
+#include <G4UIcmdWithAString.hh>
+#include <G4UIdirectory.hh>
+#include <G4UImessenger.hh>
+#include <G4UniformMagField.hh>
+
+class DetectorConstructionMessenger final : public G4UImessenger {
+public:
+  DetectorConstructionMessenger(DetectorConstruction *detectorConstruction);
+
+  void SetNewValue(G4UIcommand *command, G4String newValue) override;
+
+private:
+  DetectorConstruction *fDetectorConstruction;
+
+  std::unique_ptr<G4UIdirectory> fDirectory;
+  std::unique_ptr<G4UIcmdWithAString> fGDMLfile;
+  std::unique_ptr<G4UIcmdWith3VectorAndUnit> fMagneticField;
+};
+
+DetectorConstructionMessenger::DetectorConstructionMessenger(
+    DetectorConstruction *detectorConstruction)
+    : fDetectorConstruction(detectorConstruction) {
+  fDirectory.reset(new G4UIdirectory("/detector/"));
+  fDirectory->SetGuidance("Set detector parameters.");
+
+  fGDMLfile.reset(new G4UIcmdWithAString("/detector/gdml", this));
+  fGDMLfile->SetGuidance("Read geometry from GDML file.");
+
+  fMagneticField.reset(new G4UIcmdWith3VectorAndUnit("/detector/field", this));
+  fMagneticField->SetGuidance("Set the constant magenetic field vector.");
+  fMagneticField->SetUnitCategory("Magnetic flux density");
+  fMagneticField->SetDefaultUnit("tesla");
+}
+
+void DetectorConstructionMessenger::SetNewValue(G4UIcommand *command,
+                                                G4String newValue) {
+  if (command == fGDMLfile.get()) {
+    fDetectorConstruction->fGDMLfile = newValue;
+  } else if (command == fMagneticField.get()) {
+    G4ThreeVector magneticField = fMagneticField->GetNew3VectorValue(newValue);
+    fDetectorConstruction->fMagneticField = magneticField;
+  }
+}
+
+DetectorConstruction::DetectorConstruction() {
+  fMessenger.reset(new DetectorConstructionMessenger(this));
+}
 
 DetectorConstruction::~DetectorConstruction() {}
 
 G4VPhysicalVolume *DetectorConstruction::Construct() {
-  // Expect the user to issue "/persistency/gdml/read". If Geant4 calls us here,
-  // this didn't happen.
-  G4Exception("DetectorConstruction::Construct", "0000", FatalException,
-              "Geometry was not loaded via /persistency/gdml/read !");
-  return nullptr;
+  if (fGDMLfile.empty()) {
+    G4Exception("DetectorConstruction::Construct", "0000", FatalException,
+                "GDML file was not set via /detector/gdml !");
+    return nullptr;
+  }
+
+  G4GDMLParser parser;
+  parser.Read(fGDMLfile, true);
+  return parser.GetWorldVolume();
+}
+
+void DetectorConstruction::ConstructSDandField() {
+  G4cout << G4endl;
+  if (fMagneticField == G4ThreeVector()) {
+    G4cout << " *** No field set *** " << G4endl;
+  } else {
+    G4cout << " *** Setting constant magnetic field: [ ";
+    G4cout << fMagneticField.x() / tesla << ", " << fMagneticField.y() / tesla;
+    G4cout << ", " << fMagneticField.z() / tesla << " ] T *** " << G4endl;
+
+    auto field = new G4UniformMagField(fMagneticField);
+    G4FieldManager *fieldManager =
+        G4TransportationManager::GetTransportationManager()->GetFieldManager();
+    fieldManager->SetDetectorField(field);
+    fieldManager->CreateChordFinder(field);
+  }
 }
